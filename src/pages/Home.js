076b80f4c3c4cd7b48bc119c9b06ace1572870aa
TehreemArshad <tehreem6arshad@gmail.com>
2021-12-01 01:22:53 +0530

    Added layout management and created title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { MainPageLayout } from '../components/MainPageLayout';
 
 export const Home = () => {
-  return <div>this is home</div>;
+  return <MainPageLayout>this is home</MainPageLayout>;
+  // whatever goes inside this tag will be a part of mainpagelayout
+  // we have used children property in that
 };
