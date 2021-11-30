4aad5b9491742cc1602259870b563f024d3d6c56
TehreemArshad <tehreem6arshad@gmail.com>
2021-12-01 03:22:49 +0530

    created input and fetch data from tvmaze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MainPageLayout } from '../components/MainPageLayout';
 
 export const Home = () => {
-  return <MainPageLayout>this is home</MainPageLayout>;
+  const [input, setInput] = useState('');
+  const onInputChange = ev => {
+    // ev object
+
+    setInput(ev.target.value);
+  };
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=men
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+  const onKeyDown = ev => {
+    if (ev.keycode === 13) {
+      onSearch();
+    } // console.log(ev.keycode);
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        {' '}
+        Search
+      </button>
+    </MainPageLayout>
+  );
   // whatever goes inside this tag will be a part of mainpagelayout
   // we have used children property in that
+  // value prop inside input element is to directly associate input state with the input element.
 };
