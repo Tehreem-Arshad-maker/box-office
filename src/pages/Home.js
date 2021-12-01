cd3ff3e0c4258d6a274c2c862c480a91631d478d
TehreemArshad <tehreem6arshad@gmail.com>
2021-12-01 20:00:26 +0530

    displayed show results and small refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,43 @@
 import React, { useState } from 'react';
 import { MainPageLayout } from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 export const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
   const onInputChange = ev => {
     // ev object
 
     setInput(ev.target.value);
   };
   const onSearch = () => {
-    // https://api.tvmaze.com/search/shows?q=men
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+      // https://api.tvmaze.com/search/shows?q=men
+
+      // console.log(result);
+    });
   };
   const onKeyDown = ev => {
-    if (ev.keycode === 13) {
+    if (ev.keyCode === 13) {
       onSearch();
     } // console.log(ev.keycode);
   };
-
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div> No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
   return (
     <MainPageLayout>
       <input
@@ -34,6 +50,7 @@ export const Home = () => {
         {' '}
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
   // whatever goes inside this tag will be a part of mainpagelayout
